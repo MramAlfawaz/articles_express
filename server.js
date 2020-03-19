7ce145ccdf5e3e60de9c88b34a12b04e970dc713
schnecke <eiweala@live.com>
2020-03-19 10:18:20 +0300

    added ejs

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const PORT = 4000;
+const expressLayouts = require("express-ejs-layouts");
 
 //---initial express
 const app = express();
-
+/*
+look in views folder for a file named
+layout.ejs
+*/
+app.use(expressLayouts);
+/* will tell nodejs to look in a folder
+ called views for all ejs files */
+app.set("view engine", "ejs");
 //--mongodb connection
 mongoose.connect(
   "mongodb://localhost/articles",
@@ -15,7 +23,11 @@ mongoose.connect(
 );
 //http://google.com - GET
 app.get("/", (request, response) => {
-  response.send("This is the Home route");
+  response.render("index");
+});
+
+app.get("/a", (request, response) => {
+  response.render("another");
 });
 
 app.listen(PORT, () => console.log(`running on ${PORT}`));
